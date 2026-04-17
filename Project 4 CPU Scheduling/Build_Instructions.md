688614e0f3a67bf2c55492956177b5d0f3531d84
David <David.Ca3545@gmail.com>
2026-04-16 18:50:51 -0700

    The rest of the files

---
+++ Project 4 CPU Scheduling/Build_Instructions.md
@@ -0,0 +1,4 @@
+# How to Compile & Run Project 3
+**NOTE: Ran on UNIX or Linux Distribution. Can be ran on Windows OS as well.** 
+```bash
+```
---
+++ Project 4 CPU Scheduling/inc/CPU.h
@@ -0,0 +1,6 @@
+#include "list.h"
+// length of a time quantum
+#define QUANTUM 10
+
+// run the specified task for the following time slice
+void run(Task *task, int slice);
